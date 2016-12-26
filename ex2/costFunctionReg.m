a39e0b99cc327d201bfadf6fc25c3f140f17540b
unknown <jojo13572001@yahoo.com.tw>
2016-12-27 01:17:22 +0800

    finish logistic regression with regularization

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -8,7 +8,7 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
+%J = 0;
 grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
@@ -16,7 +16,13 @@ grad = zeros(size(theta));
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
+sigmoid_theta_X = sigmoid(X*theta);
+J = sum(-y.*log(sigmoid_theta_X) - (1-y).*log(1-sigmoid_theta_X))/m + (lambda./(2*m))*sum(theta(2:end).^2);
 
+grad(1) = sum((sigmoid_theta_X-y).*X(:,1))/m;
+for theta_iter = 2:size(theta)
+    grad(theta_iter) = sum((sigmoid_theta_X-y).*X(:,theta_iter))/m + lambda/m*theta(theta_iter);
+end
 
 
 
